00f940ba246db6bd250a1d16abec9644dd022eae
Kanad <codewizardkanad@gmail.com>
2025-02-24 23:44:09 +0530

    Added the schema file

--- src/app/documents/(document)/page.tsx
+++ src/app/documents/(document)/page.tsx
@@ -7,6 +7,11 @@ import { api } from "../../../../convex/_generated/api";
 
 const DocumentsPage = () => {
   const documents = useQuery(api.documents.get);
+  if (documents === undefined) {
+    return (
+      <div className="w-full h-screen grid place-items-center">Loading ...</div>
+    );
+  }
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed p-4 top-0 left-0 right-0 z-10 h-16 bg-white">
