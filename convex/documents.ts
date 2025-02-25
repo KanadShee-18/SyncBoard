d5e351e0e4dbff33912fb4a3217aa432e8bbf45c
Kanad <codewizardkanad@gmail.com>
2025-02-25 19:57:45 +0530

    Added the room authentication for users collaboration

--- convex/documents.ts
+++ convex/documents.ts
@@ -101,7 +101,9 @@ export const removeById = mutation({
     }
 
     const isOwner = document.ownerId === user.subject;
-    const isOrganizationMember = document?.organizationId === organizationId;
+    const isOrganizationMember = !!(
+      document?.organizationId && document?.organizationId === organizationId
+    );
 
     if (!isOwner && !isOrganizationMember) {
       throw new ConvexError("Unauthorized!");
@@ -134,12 +136,27 @@ export const updateById = mutation({
     }
 
     const isOwner = document.ownerId === user.subject;
-    const isOrganizationMember = document?.organizationId === organizationId;
+    const isOrganizationMember = !!(
+      document?.organizationId && document?.organizationId === organizationId
+    );
 
     if (!isOwner && !isOrganizationMember) {
       throw new ConvexError("Unauthorized!");
     }
 
     return await ctx.db.patch(args.id, { title: args.title });
+  },
+});
+
+export const getById = query({
+  args: { id: v.id("documents") },
+  handler: async (ctx, { id }) => {
+    const document = await ctx.db.get(id);
+
+    if (!document) {
+      throw new ConvexError("Document not found!");
+    }
+
+    return document;
   },
 });
--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -12,11 +12,7 @@ export function Room({ children }: { children: ReactNode }) {
   const params = useParams();
 
   return (
-    <LiveblocksProvider
-      publicApiKey={
-        "pk_dev_n5sV4QfQZsjq3R05EBhw5d3HE32yKnP2wJ7rNbxMqLA2gt1cug8i7iHw0Skh70kh"
-      }
-    >
+    <LiveblocksProvider throttle={16} authEndpoint={"/api/liveblocks-auth"}>
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense fallback={<div>Loading…</div>}>
           {children}
